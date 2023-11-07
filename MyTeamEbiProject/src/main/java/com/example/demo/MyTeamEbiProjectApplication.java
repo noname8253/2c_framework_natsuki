5dac8534871648efad5648cd7449b0ece318855a
Natsuki <s.natsuki.4235@gmail.com>
2023-11-07 14:10:00 +0900

    teamebi

---
+++ MyTeamEbiProject/src/main/java/com/example/demo/MyTeamEbiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTeamEbiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTeamEbiProjectApplication.class, args);
+	}
+
+}
